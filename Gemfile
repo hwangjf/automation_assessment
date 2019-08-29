7c72f5a61b63966c6b48060b2ac057e0b070f7f3
hwangjf <jeffreyhwang90@gmail.com>
2019-08-29 11:46:42 -0400

    added config

--- Gemfile
+++ Gemfile
@@ -1,9 +1,7 @@
-# frozen_string_literal: true
-
 source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-# gem "rails"
 gem 'rspec', '~> 3.5'
 gem 'selenium-webdriver'
+gem 'rspec_junit_formatter'
--- spec/amazon_search_spec.rb
+++ spec/amazon_search_spec.rb
@@ -3,11 +3,13 @@ require './test.rb'
 require 'selenium-webdriver'
 
 describe 'Amazon_search' do
+  
   describe "#score" do
     it "returns 0 for an all gutter game" do
-      game = Game.new
+      
       20.times { game.roll(0) }
       expect(game.score).to eq(0)
     end
   end
+
 end
